4464f292c5fa78555080d5acf3514ae69ca6cb66
Sting <68888117+xSting7@users.noreply.github.com>
2025-03-03 22:50:54 +1100

    Updated version

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     name = 'ProductReuploader',
-    version = '0.0.2',
+    version = '0.0.3',
     description = 'Pass and Product reuploader for Roblox universes',
 )
